71daddaa633050c729be2213d9d17ad9142c4810
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-25 18:53:16 +0200

    feat: add image domains for oauth providers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"], // Add more domains if necessary (e.g., Google OAuth)
+  },
+};
 
 export default nextConfig;
