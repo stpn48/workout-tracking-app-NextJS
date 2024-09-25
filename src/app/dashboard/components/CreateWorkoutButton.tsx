0d04de8de0faa8cdb49845f51785bf47800bfdbe
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-25 18:53:28 +0200

    Create CreateWorkoutButton.tsx

---
+++ src/app/dashboard/components/CreateWorkoutButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { signOut } from "@/app/actions/auth";
+import { Button } from "@/app/components/Button";
+import { LoadingSpinner } from "@/app/components/LoadingSpinner";
+import { useRouter } from "next/navigation";
+import React, { useCallback, useTransition } from "react";
+import toast from "react-hot-toast";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  className?: string;
+};
+
+export function CreateWorkoutButton({ className }: Props) {
+  const [addingToUrl, startAddingToUrl] = useTransition();
+
+  const router = useRouter();
+
+  const handleClick = useCallback(() => {
+    startAddingToUrl(() => {
+      router.replace("?creatingWorkout=true");
+    });
+  }, [router]);
+
+  return (
+    <Button className={twMerge("", className)} onClick={handleClick}>
+      <div className="flex items-center justify-center gap-2">
+        {addingToUrl && <LoadingSpinner className="border-t-black" />}
+        <p>CreateWorkout</p>
+      </div>
+    </Button>
+  );
+}
