148cf1224b5bc262e972c4fcb9196d5832020a1f
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-25 18:53:26 +0200

    Create ModalBackDrop.tsx

---
+++ src/app/components/ModalBackDrop.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+};
+
+export function ModalBackDrop({ children, className, onClick }: Props) {
+  return (
+    <div
+      className={twMerge(
+        "fixed inset-0 z-10 flex h-screen w-screen items-center justify-center bg-stone-900 bg-opacity-50",
+        className,
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+}
