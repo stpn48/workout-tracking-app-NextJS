fc94dd3bde70189bb3ef7e5871cc15472e33b7c6
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-25 06:54:26 +0200

    Create Input.tsx

---
+++ src/app/components/Input.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  className?: string;
+  name?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  disabled?: boolean;
+  type?: string;
+};
+
+export function Input({ className, name, type, onChange, placeholder, disabled }: Props) {
+  return (
+    <input
+      type="text"
+      className={twMerge(
+        "main-border-color rounded-md border bg-inherit px-2 py-1 outline-none",
+        className,
+        disabled && "opacity-50",
+      )}
+      name={name}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
+}
