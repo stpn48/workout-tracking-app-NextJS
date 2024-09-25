59ea04c4cad5e88a8819398811eb461a6b36a1c1
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-25 18:53:24 +0200

    Update Button.tsx

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -6,11 +6,22 @@ type Props = {
   children: React.ReactNode;
   className?: string;
   type?: "button" | "submit" | "reset";
+  disabled?: boolean;
 };
 
-export function Button({ onClick, children, className, type = "button" }: Props) {
+export function Button({ onClick, children, className, type = "button", disabled }: Props) {
   return (
-    <button onClick={onClick} className={twMerge("rounded-sm bg-white px-2 py-1 text-black", className)} type={type}>
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={twMerge(
+        "rounded-sm bg-white px-2 py-1 text-black hover:bg-opacity-90",
+        className,
+        disabled && "opacity-50",
+        disabled && "cursor-not-allowed",
+      )}
+      type={type}
+    >
       {children}
     </button>
   );
