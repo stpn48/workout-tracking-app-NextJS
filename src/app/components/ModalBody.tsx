e0389cc3e9add36882664d78c692c0d19c9eb7aa
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-25 18:53:27 +0200

    Create ModalBody.tsx

---
+++ src/app/components/ModalBody.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+};
+
+export function ModalBody({ children, className, onClick }: Props) {
+  return (
+    <div
+      className={twMerge("secondary-bg main-border-color h-[95%] w-[80%] rounded-lg border p-4 shadow-md", className)}
+      onClick={(e) => e.stopPropagation()}
+    >
+      {children}
+    </div>
+  );
+}
