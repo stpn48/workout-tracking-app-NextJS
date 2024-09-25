f3c6b87edf0234a92f6865a703d83dece86f7d0d
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-25 18:53:22 +0200

    Update auth.ts

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -4,6 +4,10 @@ import { createClient } from "@/utils/supabase/server";
 import { Provider } from "@supabase/supabase-js";
 
 export async function createUser(email: string, password: string) {
+  if (!email || !password) {
+    return { error: "Unexpected error. Email and password are required." };
+  }
+
   const supabase = createClient();
 
   const { error } = await supabase.auth.signUp({
@@ -43,14 +47,20 @@ export async function loginUser(email: string, password: string) {
 export async function signInWithProvider(provider: Provider) {
   const supabase = createClient();
 
-  const { error } = await supabase.auth.signInWithOAuth({
+  const { error, data } = await supabase.auth.signInWithOAuth({
     provider: provider,
-    options: { redirectTo: `${process.env.NEXT_BASE_URL}/api/auth` }, //TODO: Create this api route. (supabase docs)
+    options: { redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth` }, //TODO: Create this api route. (supabase docs)
   });
 
   if (error) {
-    return { error: error.message };
+    return { error: error.message, url: null };
   }
 
-  return { error: null };
+  return { error: null, url: data.url };
+}
+
+export async function signOut() {
+  const supabase = createClient();
+
+  await supabase.auth.signOut();
 }
