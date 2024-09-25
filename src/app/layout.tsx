f5a4969087b2e59ee5b1e3485cd7c52869ef9848
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-25 18:53:31 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,7 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-geistSans antialiased`}>{children}</body>
+      <body className={`${geistMono.variable} ${geistSans.variable} antialiased`}>
+        {children}
+        <Toaster toastOptions={{ style: { textAlign: "center" } }} />
+      </body>
     </html>
   );
 }
