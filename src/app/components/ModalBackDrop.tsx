d8863ce8bda84871d0a90e99b85bf9d8ce5f81e0
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-26 19:14:29 +0200

    Update ModalBackDrop.tsx

--- src/app/components/ModalBackDrop.tsx
+++ src/app/components/ModalBackDrop.tsx
@@ -5,16 +5,17 @@ type Props = {
   children: React.ReactNode;
   className?: string;
   onClick?: () => void;
+  disabled?: boolean;
 };
 
-export function ModalBackDrop({ children, className, onClick }: Props) {
+export function ModalBackDrop({ children, className, onClick, disabled }: Props) {
   return (
     <div
       className={twMerge(
         "fixed inset-0 z-10 flex h-screen w-screen items-center justify-center bg-stone-900 bg-opacity-50",
         className,
       )}
-      onClick={onClick}
+      onClick={!disabled ? onClick : undefined}
     >
       {children}
     </div>
