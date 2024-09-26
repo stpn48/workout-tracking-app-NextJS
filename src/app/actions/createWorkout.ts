e015dbdc89a7aec10c11a45b637bdb21f830bc13
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-26 19:14:28 +0200

    Create createWorkout.ts

---
+++ src/app/actions/createWorkout.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { createClient } from "@/utils/supabase/server";
+import { Difficulty } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+export async function createWorkout(formData: FormData) {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return { error: "Only authenticated users can create workouts." };
+  }
+
+  const name = formData.get("name") as string;
+  const description = formData.get("description") as string;
+  const estimatedTime = formData.get("estimatedTime") as string;
+  const difficultyString = formData.get("difficulty") as string;
+
+  const difficulty = difficultyString.toUpperCase() as Difficulty;
+
+  if (!name || !description || !estimatedTime || !difficulty) {
+    return { error: "Unexpected error. Please fill out all fields" };
+  }
+
+  try {
+    await prisma.workout.create({
+      data: {
+        name,
+        description,
+        estimated_time: parseInt(estimatedTime),
+        difficulty,
+        exercises: undefined,
+        author_id: user.id,
+      },
+    });
+
+    return { error: null };
+  } catch (error: any) {
+    return { error: error.message };
+  }
+}
