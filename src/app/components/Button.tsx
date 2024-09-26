ae4504881b12e90550706d4bad0b8637ea5ac189
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-26 06:50:29 +0200

    Update Button.tsx

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -7,15 +7,17 @@ type Props = {
   className?: string;
   type?: "button" | "submit" | "reset";
   disabled?: boolean;
+  variant?: "primary" | "secondary";
 };
 
-export function Button({ onClick, children, className, type = "button", disabled }: Props) {
+export function Button({ onClick, children, className, type = "button", disabled, variant = "primary" }: Props) {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
       className={twMerge(
-        "rounded-sm bg-white px-2 py-1 text-black hover:bg-opacity-90",
+        variant === "primary" && "rounded-sm bg-white px-2 py-1 text-black hover:bg-opacity-90",
+        variant === "secondary" && "rounded-sm bg-[#363636] px-2 py-1 text-white hover:bg-opacity-90",
         className,
         disabled && "opacity-50",
         disabled && "cursor-not-allowed",
