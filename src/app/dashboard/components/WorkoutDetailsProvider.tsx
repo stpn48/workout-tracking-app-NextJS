1cacffea603dc33fed0d500cb65f744f641c9e3c
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-27 23:36:32 +0200

    Update WorkoutDetailsProvider.tsx

--- src/app/dashboard/components/WorkoutDetailsProvider.tsx
+++ src/app/dashboard/components/WorkoutDetailsProvider.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { EditWorkoutModal } from "./EditWorkoutModal";
 import { findWorkoutDetails } from "@/app/actions/findWorkoutDetails";
+import { ErrorHandler } from "@/app/components/ErrorHandler";
+import { LoadingSpinner } from "@/app/components/LoadingSpinner";
+import { ModalBackDrop } from "@/app/components/ModalBackDrop";
+import { ModalBody } from "@/app/components/ModalBody";
 
-export async function WorkoutDetailsProvider() {
+type Props = {
+  workoutId: string;
+};
+
+export async function WorkoutDetailsProvider({ workoutId }: Props) {
   //TODO: Get workout details on server, pass search params from parent page
 
-  //Add better error handling
+  if (!workoutId) {
+    return (
+      <ModalBackDrop>
+        <ModalBody className="relative flex p-0">
+          <LoadingSpinner />
+        </ModalBody>
+      </ModalBackDrop>
+    );
+  }
 
-  const workoutDetails = await findWorkoutDetails(workoutId);
+  const { error, data: workoutDetails } = await findWorkoutDetails(workoutId);
 
-  return <EditWorkoutModal workoutDetails={workoutDetails} />;
+  return (
+    <>
+      <ErrorHandler error={error} successMsg="Workout details loaded" />
+      <EditWorkoutModal workoutDetails={workoutDetails} />
+    </>
+  );
 }
