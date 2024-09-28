218e52628d266ab97bc0f9117e72a0fd0d936a22
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-28 19:40:00 +0200

    Update getWorkoutDetails.ts

--- src/app/actions/getWorkoutDetails.ts
+++ src/app/actions/getWorkoutDetails.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import prisma from "@/lib/prisma";
-import { WorkoutDetails } from "@/types/type";
+import { ExerciseDetails, WorkoutDetails } from "@/types/type";
 import { createClient } from "@/utils/supabase/server";
 
 export async function getWorkoutDetails(workoutId: string) {
@@ -27,11 +27,23 @@ export async function getWorkoutDetails(workoutId: string) {
     },
   });
 
+  let finaleExercises: ExerciseDetails[] = [];
+
+  for (const exercise of exercises) {
+    const sets = await prisma.set.findMany({
+      where: {
+        exercise_id: exercise.id,
+      },
+    });
+
+    finaleExercises.push({ ...exercise, sets });
+  }
+
   if (!workoutDetails) {
     return { data: null, error: "Workout not found" };
   }
 
-  const finalWorkoutDetails: WorkoutDetails = { ...workoutDetails, exercises: exercises };
+  const finalWorkoutDetails: WorkoutDetails = { ...workoutDetails, exercises: finaleExercises };
 
   return { data: finalWorkoutDetails, error: null };
 }
