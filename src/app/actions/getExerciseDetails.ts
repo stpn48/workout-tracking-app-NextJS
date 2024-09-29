648c3de0a417c5b887fdbf8c6660998098b049db
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-29 13:19:49 +0200

    Create getExerciseDetails.ts

---
+++ src/app/actions/getExerciseDetails.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { createClient } from "@/utils/supabase/server";
+
+export async function getExerciseDetails(exerciseId: string, workoutId: string) {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!workoutId || !user || !exerciseId) {
+    throw new Error("Invalid input");
+  }
+
+  const workoutDetails = await prisma.workout.findUnique({
+    where: {
+      id: workoutId,
+    },
+  });
+
+  if (!workoutDetails) {
+    throw new Error("Workout not found");
+  }
+
+  if (workoutDetails.author_id !== user.id) {
+    throw new Error("Unauthorized");
+  }
+
+  const exercise = await prisma.exercise.findUnique({
+    where: {
+      id: exerciseId,
+    },
+  });
+
+  const sets = await prisma.set.findMany({
+    where: {
+      exercise_id: exerciseId,
+    },
+  });
+
+  return { ...exercise, sets };
+}
