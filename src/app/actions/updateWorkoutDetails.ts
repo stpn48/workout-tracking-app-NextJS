0bc0366b5e5ac7cce01b1c0c52bad261c1c43d6f
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-29 13:19:53 +0200

    Create updateWorkoutDetails.ts

---
+++ src/app/actions/updateWorkoutDetails.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+type Props = {
+  newName: string;
+  newDescription: string;
+  newEstimatedTime: string;
+  workoutId: string;
+};
+
+export async function updateWorkoutDetails({ newName, newDescription, newEstimatedTime, workoutId }: Props) {
+  if (!newName || !newDescription || !newEstimatedTime) {
+    return { error: "Unexpected Error. Please fill in all fields" };
+  }
+
+  let newEstimatedTimeNumber;
+
+  try {
+    newEstimatedTimeNumber = parseInt(newEstimatedTime);
+  } catch (e) {
+    return { error: "Unexpected Error. Please enter a valid estimated time" };
+  }
+
+  await prisma.workout.update({
+    where: {
+      id: workoutId,
+    },
+    data: {
+      name: newName,
+      description: newDescription,
+      estimated_time: newEstimatedTimeNumber,
+    },
+  });
+
+  revalidatePath("/workout");
+  return { error: null };
+}
