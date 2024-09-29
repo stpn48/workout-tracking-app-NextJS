d88d934eedbd2c901ffc440f3bdb283d049bc29c
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-29 13:19:52 +0200

    Create getWorkoutExercises.ts

---
+++ src/app/actions/getWorkoutExercises.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { ExerciseDetails } from "@/types/type";
+import { getUser } from "@/utils/supabase/server";
+
+export async function getWorkoutExercises(workoutId: string) {
+  const user = await getUser();
+
+  if (!workoutId || !user) {
+    throw new Error("Invalid request");
+  }
+
+  const workoutDetails = await prisma.workout.findUnique({
+    where: {
+      id: workoutId,
+    },
+  });
+
+  if (!workoutDetails) {
+    throw new Error("Workout not found");
+  }
+
+  if (workoutDetails.author_id !== user.id) {
+    throw new Error("Unauthorized");
+  }
+
+  // all security check validations passed
+  const exercises = await prisma.exercise.findMany({
+    where: {
+      workout_id: workoutId,
+    },
+  });
+
+  let finaleExercises: ExerciseDetails[] = [];
+
+  for (const exercise of exercises) {
+    const sets = await prisma.set.findMany({
+      where: {
+        exercise_id: exercise.id,
+      },
+    });
+
+    finaleExercises.push({ ...exercise, sets });
+  }
+
+  return finaleExercises;
+}
