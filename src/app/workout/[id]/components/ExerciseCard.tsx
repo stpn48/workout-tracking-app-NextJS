1ad3830af4f7145f1ed86391a26f2c5b9d7f4920
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-30 19:50:40 +0200

    Update ExerciseCard.tsx

--- src/app/workout/[id]/components/ExerciseCard.tsx
+++ src/app/workout/[id]/components/ExerciseCard.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import { ExerciseDetails } from "@/types/type";
+import { Exercise, Set } from "@prisma/client";
 import React from "react";
 
 type Props = {
-  exercise: ExerciseDetails;
+  exercise: Exercise & { sets: Set[] };
   onClick?: () => void;
+  order: number;
 };
 
-export function ExerciseCard({ exercise, onClick }: Props) {
+export function ExerciseCard({ exercise, onClick, order }: Props) {
   return (
     <div
       onClick={onClick}
-      className="main-border-color w-[250px] rounded-lg border px-6 py-4 text-sm"
+      className="main-border-color relative w-[250px] rounded-lg border px-6 py-4 text-sm"
     >
+      <p className="text-secondary absolute left-2 top-2 text-xs">{order}</p>
       <h1 className="mb-4 flex w-full justify-center text-lg font-bold">{exercise.name}</h1>
       <div className="flex flex-col gap-2">
         <div className="text-secondary flex justify-between rounded-lg text-xs font-bold">
