227fd8e0880499f4028a1e7bd2df70792ba034b8
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-01 06:09:50 +0200

    Update useExercises.ts

--- src/hooks/useExercises.ts
+++ src/hooks/useExercises.ts
@@ -7,5 +7,9 @@ export function useExercises(workoutId: string) {
   return useQuery({
     queryKey: ["workoutExercises", { workoutId }],
     queryFn: () => getWorkoutExercises(workoutId),
+    staleTime: 5 * 60 * 1000, // 5 minutes of freshness (adjust as needed)
+    refetchOnWindowFocus: false, // Disable refetching on window focus
+    refetchOnMount: false, // Prevent refetching when remounting
+    refetchOnReconnect: true, // Optional, only refetch when reconnecting to the internet
   });
 }
