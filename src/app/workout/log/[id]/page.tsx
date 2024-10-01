99f4f8092721e7be7f4c6714d66469a8c680e95d
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-01 06:09:50 +0200

    Create page.tsx

---
+++ src/app/workout/log/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { getWorkoutExercises } from "@/app/actions/getWorkoutExercises";
+import { H1 } from "@/app/components/H1";
+import Link from "next/link";
+import { ExerciseList } from "./components/ExerciseList";
+
+export default async function LogWorkoutPage({ params }: { params: { id: string } }) {
+  if (!params.id) {
+    return null;
+  }
+
+  const exercises = await getWorkoutExercises(params.id);
+
+  return (
+    <div className="main-bg min-h-screen w-screen p-4 text-sm text-white">
+      <Link className="text-base" href={"/dashboard"}>
+        Go Back
+      </Link>
+      <H1>Log Workout</H1>
+      <ExerciseList exercises={exercises} />
+    </div>
+  );
+}
