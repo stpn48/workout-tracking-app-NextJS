75599933922275e0e0dd04fdf6c374cd1e1e3cf1
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-02 20:29:31 +0200

    Create page.tsx

---
+++ src/app/dashboard/completedExercises/page.tsx
@@ -0,0 +1,23 @@
+import { CompletedExercisesList } from "./components/CompletedExercisesList";
+import { getCompletedExercises } from "@/app/actions/getCompletedExercises";
+import { ToastMsgHandler } from "@/app/components/ToastMsgHandler";
+import React, { useState } from "react";
+
+export default async function CompletedExercisesPage() {
+  let errorMessage = "";
+  let completedExercises;
+
+  try {
+    completedExercises = await getCompletedExercises();
+  } catch (e: unknown) {
+    errorMessage = e as string;
+    return;
+  }
+
+  return (
+    <>
+      {errorMessage && <ToastMsgHandler msg={errorMessage} type="error" />}
+      <CompletedExercisesList completedExercises={completedExercises} />
+    </>
+  );
+}
