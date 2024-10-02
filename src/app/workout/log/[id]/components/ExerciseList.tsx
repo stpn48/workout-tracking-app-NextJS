88b73457faaa4ba7c8aeec613efff1c393854371
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-02 20:29:35 +0200

    Update ExerciseList.tsx

--- src/app/workout/log/[id]/components/ExerciseList.tsx
+++ src/app/workout/log/[id]/components/ExerciseList.tsx
@@ -5,31 +5,44 @@ import React, { useCallback, useEffect, useState } from "react";
 import { LogExerciseCard } from "./LogExerciseCard";
 import { Button } from "@/app/components/Button";
 import toast from "react-hot-toast";
-import { logWorkout } from "@/app/actions/LogWorkout";
+import { logWorkout } from "@/app/actions/logWorkout";
 
 type Props = {
   exercises: ExerciseDetails[];
 };
 
 export function ExerciseList({ exercises }: Props) {
-  const [effortsPerExercise, setEffortsPerExercise] = useState<{ [exerciseId: string]: number[] }>(
-    {},
-  );
+  const [effortsPerExercise, setEffortsPerExercise] = useState<{
+    [exerciseId: string]: { [setId: string]: number };
+  }>({}); // each exercise id has a object of set id's and each set id has the effort
 
   const handleFinishClick = useCallback(async () => {
     // check if all sets are filled
+
+    if (Object.keys(effortsPerExercise).length !== exercises.length) {
+      toast.error("Please complete all sets for each exercise 1");
+      return;
+    }
+
     for (const exerciseId of Object.keys(effortsPerExercise)) {
       if (
-        effortsPerExercise[exerciseId].length !==
+        Object.keys(effortsPerExercise[exerciseId]).length !==
         exercises.find((exercise) => exercise.id === exerciseId)!.sets.length
       ) {
-        toast.error("Please complete all sets for each exercise");
+        toast.error("Please complete all sets for each exercise 2");
         return;
       }
     }
+    toast.success("All fields filled");
 
-    await logWorkout(effortsPerExercise);
-  }, [effortsPerExercise]);
+    try {
+      await logWorkout(effortsPerExercise);
+      toast.success("Workout logged successfully");
+    } catch (error: any) {
+      toast.error("Failed to log workout", error);
+      return;
+    }
+  }, [effortsPerExercise, exercises]);
 
   useEffect(() => {
     console.log(effortsPerExercise);
