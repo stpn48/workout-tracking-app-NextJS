de14e0ef37a3cac3d8c3388bb76517db2638eb05
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-02 20:29:30 +0200

    Create MoreDetailsModal.tsx

---
+++ src/app/dashboard/completedExercises/components/MoreDetailsModal.tsx
@@ -0,0 +1,22 @@
+import { ModalBackDrop } from "@/app/components/ModalBackdrop";
+import { ModalBody } from "@/app/components/ModalBody";
+import { CompletedExercise } from "@prisma/client";
+import React from "react";
+
+type Props = {
+  completedExercise: CompletedExercise;
+  exerciseName: string;
+  closeModal: () => void;
+};
+
+export default function MoreDetailsModal({ completedExercise, exerciseName, closeModal }: Props) {
+  return (
+    <ModalBackDrop onClick={closeModal}>
+      <ModalBody className="text-white">
+        <h1>{exerciseName}</h1>
+        <h1>times completed: {completedExercise.timesCompleted}</h1>
+        <h1>effort history: {JSON.stringify(completedExercise.maxRepsHistory)}</h1>
+      </ModalBody>
+    </ModalBackDrop>
+  );
+}
