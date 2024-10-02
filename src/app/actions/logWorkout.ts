e2e6967aee682df68ed19566942e774154d9524c
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-02 20:29:27 +0200

    Update logWorkout.ts

--- src/app/actions/logWorkout.ts
+++ src/app/actions/logWorkout.ts
@@ -3,24 +3,62 @@
 import { getUser } from "@/utils/supabase/server";
 import prisma from "@/lib/prisma";
 
-export async function logWorkout(effortsPerExercise: { [exerciseId: string]: number[] }) {
+export async function logWorkout(effortsPerExercise: {
+  [exerciseId: string]: { [setId: string]: number };
+}) {
   const user = await getUser();
 
   if (!user) {
     throw new Error("User not authenticated");
   }
 
-  for (const exerciseId of Object.keys(effortsPerExercise)) {
-    const exercise = await prisma.completedExercise.findUnique({
-      where: {
-        id: exerciseId,
-      },
-    });
+  //TODO: ADD more security checks
 
-    if (!exercise) {
-      // exercise not found, create on
+  try {
+    for (const exerciseId of Object.keys(effortsPerExercise)) {
+      const exercise = await prisma.completedExercise.findFirst({
+        where: {
+          exercise_id: exerciseId,
+        },
+      });
+
+      // Calculate maxReps of this exercise
+      let maxReps = -Infinity;
+
+      for (const setId in effortsPerExercise[exerciseId]) {
+        if (effortsPerExercise[exerciseId][setId] > maxReps) {
+          maxReps = effortsPerExercise[exerciseId][setId];
+        }
+      }
+
+      if (!exercise) {
+        await prisma.completedExercise.create({
+          data: {
+            author_id: user.id,
+            exercise_id: exerciseId,
+            maxRepsHistory: [maxReps],
+          },
+        });
+        continue;
+      }
+
+      //exercise already was logged
+
+      await prisma.completedExercise.update({
+        where: {
+          id: exercise.id,
+        },
+        data: {
+          timesCompleted: exercise.timesCompleted + 1,
+          maxRepsHistory: [...(exercise.maxRepsHistory || []), maxReps],
+        },
+      });
     }
-
-    // exercise found, push new max effort
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      throw new Error(`Error logging workout: ${e.message}`);
+    } else {
+      console.error(e);
+    }
   }
 }
