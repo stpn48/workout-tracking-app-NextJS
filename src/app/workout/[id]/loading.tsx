e64b532474efe9b939835525156167efdfb22487
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:53 +0200

    Update loading.tsx

--- src/app/workout/[id]/loading.tsx
+++ src/app/workout/[id]/loading.tsx
@@ -4,9 +4,9 @@ export default function loading() {
   return (
     <div className="main-bg fixed inset-0 min-h-screen w-screen overflow-scroll p-4 text-white">
       <div className="flex flex-col gap-3">
-        <div className="h-5 w-[100px] animate-pulse rounded-md bg-stone-700" />
-        <div className="h-7 w-[200px] animate-pulse rounded-md bg-stone-700" />
-        <div className="h-5 w-[200px] animate-pulse rounded-md bg-stone-700" />
+        <div className="h-[19px] w-[100px] animate-pulse rounded-md bg-stone-700" />
+        <div className="h-[42px] w-[200px] animate-pulse rounded-md bg-stone-700" />
+        <div className="h-[24px] w-[200px] animate-pulse rounded-md bg-stone-700" />
       </div>
 
       <div className="absolute right-4 top-4 flex gap-2">
