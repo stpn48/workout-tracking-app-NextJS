280c8425f49b1d20cd02723fc11a95e538747f7d
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:55 +0200

    Update loading.tsx

--- src/app/workout/log/[id]/loading.tsx
+++ src/app/workout/log/[id]/loading.tsx
@@ -1,12 +1,18 @@
+import { LoadingSpinner } from "@/app/components/LoadingSpinner";
 import React from "react";
 
 export default function loading() {
   return (
     <div className="main-bg fixed inset-0 min-h-screen w-screen overflow-scroll p-4 text-white">
-      <div className="flex flex-col gap-3">
-        <div className="h-5 w-[100px] animate-pulse rounded-md bg-stone-700" />
-        <div className="h-7 w-[200px] animate-pulse rounded-md bg-stone-700" />
+      <div className="flex flex-col gap-1">
+        <div className="h-[19px] w-[100px] animate-pulse rounded-sm bg-stone-700" />
+        <div className="h-[42px] w-[200px] animate-pulse rounded-sm bg-stone-700" />
       </div>
+      <div className="mt-20 flex w-full justify-center">
+        <LoadingSpinner />
+      </div>
+
+      <div className="absolute bottom-4 right-4 h-[32px] w-[58px] animate-pulse rounded-sm bg-stone-700" />
     </div>
   );
 }
