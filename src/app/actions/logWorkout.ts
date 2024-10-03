1b3a0b8df93b88cc36ca3b69adad67618eac1582
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:43 +0200

    Update logWorkout.ts

--- src/app/actions/logWorkout.ts
+++ src/app/actions/logWorkout.ts
@@ -2,10 +2,16 @@
 
 import { getUser } from "@/utils/supabase/server";
 import prisma from "@/lib/prisma";
+import { Workout } from "@prisma/client";
 
-export async function logWorkout(effortsPerExercise: {
-  [exerciseId: string]: { [setId: string]: number };
-}) {
+type Props = {
+  effortsPerExercise: {
+    [exerciseName: string]: { [setId: string]: number };
+  };
+  workoutDetails: Workout;
+};
+
+export async function logWorkout({ effortsPerExercise, workoutDetails }: Props) {
   const user = await getUser();
 
   if (!user) {
@@ -15,27 +21,29 @@ export async function logWorkout(effortsPerExercise: {
   //TODO: ADD more security checks
 
   try {
-    for (const exerciseId of Object.keys(effortsPerExercise)) {
-      const exercise = await prisma.completedExercise.findFirst({
+    // Create or update completedExercises
+    for (const exerciseName of Object.keys(effortsPerExercise)) {
+      const completedExercise = await prisma.completedExercise.findFirst({
         where: {
-          exercise_id: exerciseId,
+          name: exerciseName,
         },
       });
 
       // Calculate maxReps of this exercise
       let maxReps = -Infinity;
 
-      for (const setId in effortsPerExercise[exerciseId]) {
-        if (effortsPerExercise[exerciseId][setId] > maxReps) {
-          maxReps = effortsPerExercise[exerciseId][setId];
+      for (const setId in effortsPerExercise[exerciseName]) {
+        if (effortsPerExercise[exerciseName][setId] > maxReps) {
+          maxReps = effortsPerExercise[exerciseName][setId];
         }
       }
 
-      if (!exercise) {
+      // exercise wasn't found create new completedExercise
+      if (!completedExercise) {
         await prisma.completedExercise.create({
           data: {
             author_id: user.id,
-            exercise_id: exerciseId,
+            name: exerciseName,
             maxRepsHistory: [maxReps],
           },
         });
@@ -46,14 +54,44 @@ export async function logWorkout(effortsPerExercise: {
 
       await prisma.completedExercise.update({
         where: {
-          id: exercise.id,
+          name: exerciseName,
         },
         data: {
-          timesCompleted: exercise.timesCompleted + 1,
-          maxRepsHistory: [...(exercise.maxRepsHistory || []), maxReps],
+          timesCompleted: completedExercise.timesCompleted + 1,
+          maxRepsHistory: [...(completedExercise.maxRepsHistory || []), maxReps],
         },
       });
     }
+
+    // Create or update the completedWorkout
+    const completedWorkout = await prisma.completedWorkout.findFirst({
+      where: {
+        workout_id: workoutDetails.id,
+      },
+    });
+
+    // If the workout was not found, create a new completedWorkout
+    if (!completedWorkout) {
+      await prisma.completedWorkout.create({
+        data: {
+          author_id: user.id,
+          name: workoutDetails.name,
+          workout_id: workoutDetails.id,
+          timesCompleted: 1,
+        },
+      });
+      return;
+    }
+
+    // If the workout was found, update the timesCompleted
+    await prisma.completedWorkout.update({
+      where: {
+        id: completedWorkout.id,
+      },
+      data: {
+        timesCompleted: completedWorkout.timesCompleted + 1,
+      },
+    });
   } catch (e: unknown) {
     if (e instanceof Error) {
       throw new Error(`Error logging workout: ${e.message}`);
