b4352e491d138d3d00b8fc48542a5d210b24dcba
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:42 +0200

    Update getUserWorkouts.ts

--- src/app/actions/getUserWorkouts.ts
+++ src/app/actions/getUserWorkouts.ts
@@ -2,12 +2,13 @@
 
 import { getUser } from "@/utils/supabase/server";
 import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
 
 export async function getUserWorkouts() {
   const user = await getUser();
 
   if (!user) {
-    throw new Error("User not found");
+    redirect("/login");
   }
 
   const workouts = await prisma.workout.findMany({
