7bba27e201790f62721b47051729f6c63428f4aa
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:49 +0200

    Update WorkoutCard.tsx

--- src/app/dashboard/components/WorkoutCard.tsx
+++ src/app/dashboard/components/WorkoutCard.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/app/components/Button";
 import { Workout } from "@prisma/client";
 import Link from "next/link";
 import React from "react";
@@ -9,18 +10,16 @@ type Props = {
 export function WorkoutCard({ workout }: Props) {
   return (
     <div className="main-border-color secondary-bg relative flex w-[300px] cursor-default flex-col gap-1 rounded-lg border p-4">
-      <Link className="cursor-default" href={`/workout/${workout.id}`}>
+      <Link className="cursor-pointer" href={`/workout/${workout.id}`}>
         <div className="flex flex-col">
           <h1 className="text-base">{workout.name}</h1>
           <p className="text-secondary">{workout.description}</p>
         </div>
         <p>{workout.estimated_time} min</p>
       </Link>
-      <Link
-        className="absolute bottom-4 right-4 rounded-md bg-white px-2 py-1 text-black"
-        href={`/workout/log/${workout.id}`}
-      >
-        Log
+
+      <Link className="absolute bottom-4 right-4" href={`/workout/log/${workout.id}`}>
+        <Button>Log</Button>
       </Link>
     </div>
   );
