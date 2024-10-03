a39b7afe91917cdc20a25cea12417247451a7a5c
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:48 +0200

    Update Links.tsx

--- src/app/dashboard/components/Links.tsx
+++ src/app/dashboard/components/Links.tsx
@@ -20,17 +20,17 @@ export function Links({}: Props) {
     } else if (pathname === "/dashboard/completedWorkouts") {
       setActive(3);
     }
-  }, []);
+  }, [pathname]);
 
   return (
     <div className="flex gap-4">
-      <Link href="/dashboard" onClick={() => setActive(1)}>
+      <Link href="/dashboard">
         <H1 className={`${active !== 1 && "text-[#313131]"}`}>Your Workouts</H1>
       </Link>
-      <Link href="/dashboard/completedExercises" onClick={() => setActive(2)}>
+      <Link href="/dashboard/completedExercises">
         <H1 className={`${active !== 2 && "text-[#313131]"}`}>Completed Exercises</H1>
       </Link>
-      <Link href="/dashboard/completedWorkouts" onClick={() => setActive(3)}>
+      <Link href="/dashboard/completedWorkouts">
         <H1 className={`${active !== 3 && "text-[#313131]"}`}>Completed Workouts</H1>
       </Link>
     </div>
