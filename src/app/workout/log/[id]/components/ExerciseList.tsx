656b22997afbb90b973a30be92671b64c6140b10
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:54 +0200

    Update ExerciseList.tsx

--- src/app/workout/log/[id]/components/ExerciseList.tsx
+++ src/app/workout/log/[id]/components/ExerciseList.tsx
@@ -7,14 +7,16 @@ import { Button } from "@/app/components/Button";
 import toast from "react-hot-toast";
 import { logWorkout } from "@/app/actions/logWorkout";
 import { useRouter } from "next/navigation";
+import { Workout } from "@prisma/client";
 
 type Props = {
+  workoutDetails: Workout;
   exercises: ExerciseDetails[];
 };
 
-export function ExerciseList({ exercises }: Props) {
+export function ExerciseList({ exercises, workoutDetails }: Props) {
   const [effortsPerExercise, setEffortsPerExercise] = useState<{
-    [exerciseId: string]: { [setId: string]: number };
+    [exerciseName: string]: { [setId: string]: number };
   }>({}); // each exercise id has a object of set id's and each set id has the effort
 
   const router = useRouter();
@@ -23,30 +25,37 @@ export function ExerciseList({ exercises }: Props) {
     // check if all sets are filled
 
     if (Object.keys(effortsPerExercise).length !== exercises.length) {
-      toast.error("Please complete all sets for each exercise 1");
+      toast.error("Fill all sets for each exercise ");
       return;
     }
 
-    for (const exerciseId of Object.keys(effortsPerExercise)) {
+    for (const exerciseName of Object.keys(effortsPerExercise)) {
       if (
-        Object.keys(effortsPerExercise[exerciseId]).length !==
-        exercises.find((exercise) => exercise.id === exerciseId)!.sets.length
+        Object.keys(effortsPerExercise[exerciseName]).length !==
+        exercises.find((exercise) => exercise.name === exerciseName)!.sets.length
       ) {
-        toast.error("Please complete all sets for each exercise 2");
+        toast.error("Fill all sets for each exercise ");
         return;
       }
     }
     toast.success("All fields filled");
 
     try {
-      await logWorkout(effortsPerExercise);
+      await logWorkout({ effortsPerExercise, workoutDetails });
       toast.success("Workout logged successfully");
       router.push("/dashboard/completedExercises");
     } catch (error: any) {
       toast.error("Failed to log workout", error);
       return;
     }
-  }, [effortsPerExercise, exercises]);
+  }, [effortsPerExercise, exercises, workoutDetails]);
+
+  useEffect(() => {
+    if (exercises.length === 0) {
+      router.replace("/dashboard");
+      toast.error("You need to add exercises to log a workout");
+    }
+  }, [exercises]);
 
   return (
     <div className="mt-10 flex w-full justify-center">
