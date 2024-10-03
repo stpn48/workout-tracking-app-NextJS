e9a8b2f040fa4cf54cbaf2ed491200caa3de4268
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:50 +0200

    Update loading.tsx

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -7,6 +7,8 @@ export default function loading({}: Props) {
   return (
     <div className="flex h-full w-full items-center justify-center text-white">
       <LoadingSpinner />
+      <div className="absolute right-4 top-4 h-[28px] w-[116px] animate-pulse rounded-sm bg-stone-700" />
+      <div className="absolute bottom-4 right-4 h-[28px] w-[71px] animate-pulse rounded-sm bg-stone-700" />
     </div>
   );
 }
