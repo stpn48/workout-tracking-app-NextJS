0bdd77b2d5c797febf9b5cec950286b2ac69b84a
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:47 +0200

    Update page.tsx

--- src/app/dashboard/completedWorkouts/page.tsx
+++ src/app/dashboard/completedWorkouts/page.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import prisma from "@/lib/prisma";
+import { getUser } from "@/utils/supabase/server";
+import { CompletedWorkoutCard } from "./components/CompletedWorkoutCard";
+import { comment } from "postcss";
 
-export default function CompletedWorkoutsPage() {
-  return <div>page</div>;
+export default async function CompletedWorkoutsPage() {
+  const user = await getUser();
+
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
+
+  const completedWorkouts = await prisma.completedWorkout.findMany({
+    where: {
+      author_id: user.id,
+    },
+  });
+
+  return (
+    <div>
+      <div className="mt-10 text-white">
+        {completedWorkouts.length === 0 && (
+          <p className="text-secondary flex w-full justify-center text-xs">
+            You hasn't completed any workout yet.
+          </p>
+        )}
+        {completedWorkouts.map((completedWorkout) => (
+          <CompletedWorkoutCard key={completedWorkout.id} completedWorkout={completedWorkout} />
+        ))}
+      </div>
+    </div>
+  );
 }
