04f8026757a819349e3e2890ca0561da550dc5e5
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:49 +0200

    Update WorkoutCardList.tsx

--- src/app/dashboard/components/WorkoutCardList.tsx
+++ src/app/dashboard/components/WorkoutCardList.tsx
@@ -1,8 +1,7 @@
 import { Workout } from "@prisma/client";
 import React from "react";
 import { WorkoutCard } from "./WorkoutCard";
-
-const MemoWorkoutCard = React.memo(WorkoutCard);
+import Image from "next/image";
 
 type Props = {
   workouts: Workout[];
@@ -10,9 +9,23 @@ type Props = {
 
 export function WorkoutCardList({ workouts }: Props) {
   return (
-    <div className="flex flex-wrap gap-4 pt-8">
+    <div className="mt-10 flex flex-wrap gap-4">
+      {workouts.length === 0 && (
+        <>
+          <Image
+            className="absolute right-[150px] top-[40px] rotate-6"
+            alt="arrowImg"
+            src={"/arrowImg.png"}
+            width={320}
+            height={320}
+          />
+          <p className="text-secondary mt-[80px] flex w-full justify-center text-xs">
+            You have no created workouts yet. Stat here
+          </p>
+        </>
+      )}
       {workouts.map((workout) => (
-        <MemoWorkoutCard key={workout.id} workout={workout} />
+        <WorkoutCard key={workout.id} workout={workout} />
       ))}
     </div>
   );
