44816b56452566632fdac776a60ac604b6974c3d
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:45 +0200

    Update CompletedExercisesList.tsx

--- src/app/dashboard/completedExercises/components/CompletedExercisesList.tsx
+++ src/app/dashboard/completedExercises/components/CompletedExercisesList.tsx
@@ -8,29 +8,15 @@ type Props = {
 };
 
 export async function CompletedExercisesList({ completedExercises }: Props) {
-  const exerciseNames: string[] = [];
-  for (let completedExercise of completedExercises) {
-    const exercise = await prisma.exercise.findFirst({
-      where: {
-        id: completedExercise.exercise_id,
-      },
-    });
-
-    if (!exercise) {
-      throw new Error("THIS SHOULD(WILL) NEVER HAPPEN");
-    }
-
-    exerciseNames.push(exercise.name);
-  }
   return (
     <div className="mt-10 flex flex-wrap gap-4">
+      {completedExercises.length === 0 && (
+        <p className="text-secondary flex w-full justify-center text-xs">
+          You hasn't completed any exercise yet.
+        </p>
+      )}
       {completedExercises.map((completedExercise, i) => {
-        return (
-          <CompletedExerciseCard
-            exerciseName={exerciseNames[i]}
-            completedExercise={completedExercise}
-          />
-        );
+        return <CompletedExerciseCard completedExercise={completedExercise} />;
       })}
     </div>
   );
