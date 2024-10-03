2f7a475d3fc92e985548915a7b8006200366f5f9
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-03 10:59:48 +0200

    Update SignOutButton.tsx

--- src/app/dashboard/components/SignOutButton.tsx
+++ src/app/dashboard/components/SignOutButton.tsx
@@ -2,8 +2,9 @@
 
 import { signOut } from "@/app/actions/auth";
 import { Button } from "@/app/components/Button";
+import { ConfirmationModal } from "@/app/components/ConfirmationModal";
 import { useRouter } from "next/navigation";
-import React, { useCallback, useTransition } from "react";
+import React, { useCallback, useState, useTransition } from "react";
 import toast from "react-hot-toast";
 import { twMerge } from "tailwind-merge";
 
@@ -12,21 +13,33 @@ type Props = {
 };
 
 export function SignOutButton({ className }: Props) {
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
   const [signingOut, startSigningOut] = useTransition();
-
-  const router = useRouter();
 
   const handleSignOut = useCallback(() => {
     startSigningOut(async () => {
       await signOut();
-      router.replace("/login"); // TODO: Remove this after implementing the protected route. Wont be needed.
       toast.success("Signed out successfully");
     });
   }, []);
 
   return (
-    <Button className={twMerge("", className)} disabled={signingOut} onClick={handleSignOut}>
-      Sign Out
-    </Button>
+    <>
+      <Button
+        className={twMerge("", className)}
+        disabled={signingOut}
+        onClick={() => setShowConfirmationModal(true)}
+      >
+        Sign Out
+      </Button>
+
+      {showConfirmationModal && (
+        <ConfirmationModal
+          message="Are you sure you want to sign out?"
+          onConfirm={handleSignOut}
+          onCancel={() => setShowConfirmationModal(false)}
+        />
+      )}
+    </>
   );
 }
